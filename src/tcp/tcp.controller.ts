be9484f8488b41b2cb332c4785978d45e495bf76
LuxWise <jcsanchez.martinez.2020@gmail.com>
2025-07-08 14:14:13 -0500

    feat: add timer

--- src/tcp/tcp.controller.ts
+++ src/tcp/tcp.controller.ts
@@ -2,10 +2,18 @@ import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('')
 export class TcpController {
+  private lastTime = Date.now();
+  private count = 0;
+
   @Post('data-shipper')
   handleData(@Body() data: any) {
-    let dataNumber = 0;
-    console.log(`Data-${dataNumber++}:`, data);
+    const now = Date.now();
+    const diffInSeconds = ((now - this.lastTime) / 1000).toFixed(2);
+    this.lastTime = now;
+    this.count++;
+
+    console.log(`→ #${this.count} | Tiempo desde la última: ${diffInSeconds}s`);
+    console.log(`Data:`, data);
     return { message: 'Data is recived' };
   }
 }
