6d2e2a4163fb57475bddad06280404af5c2581c8
LuxWise <jcsanchez.martinez.2020@gmail.com>
2025-07-17 18:00:06 -0500

    fix: validate simcard exists

--- src/datashipper/datashipper.service.ts
+++ src/datashipper/datashipper.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { SimCard } from 'src/sim_card/sim_card.entity';
 import { SimCardService } from 'src/sim_card/sim_card.service';
 
 @Injectable()
@@ -6,6 +7,8 @@ export class DatashipperService {
   constructor(private readonly simCardService: SimCardService) {}
 
   async insertData(data: datashiperData) {
+    const results: { action: string; sim: SimCard }[] = [];
+
     for (let i = 0; i < data.notifications.length; i++) {
       const simInfo = data.notifications[i] as {
         iccid: string;
@@ -20,11 +23,19 @@ export class DatashipperService {
         simInfo.iccid,
       );
 
-      if (!simcardExists) {
-        return this.simCardService.update(simInfo.iccid, simInfo);
-      }
+      console.log(simcardExists);
 
-      return this.simCardService.create(simInfo);
+      if (!simcardExists) {
+        const created = await this.simCardService.create(simInfo);
+        results.push({ action: 'created', sim: created });
+      } else {
+        const updated = await this.simCardService.update(
+          simInfo.iccid,
+          simInfo,
+        );
+        results.push({ action: 'updated', sim: updated });
+      }
     }
+    return results;
   }
 }
