2162d3631a87a2003dc3708bf7a500e1043e2cbe
LuxWise <jcsanchez.martinez.2020@gmail.com>
2025-07-21 16:50:04 -0500

    feat: add request headers host

--- src/datashipper/datashipper.controller.ts
+++ src/datashipper/datashipper.controller.ts
@@ -4,16 +4,21 @@ import {
   HttpException,
   HttpStatus,
   Post,
+  Req,
 } from '@nestjs/common';
 import { DatashipperService } from './datashipper.service';
+import { Request } from 'express';
 
 @Controller()
 export class DatashipperController {
   constructor(private readonly datashipperService: DatashipperService) {}
 
   @Post('data')
-  async handleData(@Body() data: DatashiperDataDTO) {
+  async handleData(@Body() data: DatashiperDataDTO, @Req() req: Request) {
     try {
+      const host = req.headers['host'];
+      console.log(`Host: ${host}`);
+
       await this.datashipperService.insertData(data);
       return { message: 'Data is recived' };
     } catch (error) {
