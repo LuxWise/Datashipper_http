94015375555d3e8c78f2b2f2b7c3c9dc1a8c9542
LuxWise <jcsanchez.martinez.2020@gmail.com>
2025-07-23 10:33:32 -0500

    feat: add test ip

--- src/datashipper/datashipper.controller.ts
+++ src/datashipper/datashipper.controller.ts
@@ -4,16 +4,25 @@ import {
   HttpException,
   HttpStatus,
   Post,
+  Req,
 } from '@nestjs/common';
 import { DatashipperService } from './datashipper.service';
+import { Request } from 'express';
 
 @Controller()
 export class DatashipperController {
   constructor(private readonly datashipperService: DatashipperService) {}
 
   @Post('data')
-  async handleData(@Body() data: DatashiperDataDTO) {
+  async handleData(@Body() data: DatashiperDataDTO, @Req() req: Request) {
     try {
+      const forwardedFor = req.headers['x-forwarded-for'];
+      const clientIp =
+        typeof forwardedFor === 'string'
+          ? forwardedFor.split(',')[0].trim()
+          : req.socket.remoteAddress;
+
+      console.log(clientIp);
       await this.datashipperService.insertData(data);
       return { message: 'Data is recived' };
     } catch (error) {
